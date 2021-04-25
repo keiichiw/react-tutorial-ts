4c21510fb7e50dc2458e23ce97b7ba678037479e
Keiichi Watanabe <udon.watanabe@gmail.com>
2021-04-25 17:46:48 +0900

    Add GitHub icon button

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,37 @@
 import ReactDOM from "react-dom";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import { AppBar, Toolbar, Tooltip, IconButton, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import Game from "./game";
 
+const useStyles = makeStyles((theme) => ({
+    toolbarButtons: {
+        marginLeft: 'auto',
+    },
+}));
+
 function Main() {
+    const classes = useStyles();
   return (
     <div>
       <div>
         <AppBar position="static">
           <Toolbar>
-            <IconButton
-              edge="start"
-              className="icon-button"
-              color="inherit"
-              aria-label="menu"
-            ></IconButton>
-            <Typography variant="h6" className="news">
+            <Typography variant="h6">
               Tic-Tac-Toe
             </Typography>
+            <div className={classes.toolbarButtons}>
+                <Tooltip title="GitHub repository">
+            <IconButton
+                aria-label="github"
+                color="inherit"
+                onClick={() => window.open('https://github.com/keiichiw/react-tutorial-ts')}
+            >
+                <GitHubIcon />
+            </IconButton>
+            </Tooltip>
+            </div>
           </Toolbar>
         </AppBar>
       </div>
